1b1a9348eaae2f41c23a19872deba04b607385db
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-02-17 16:10:28 -0500

    upload database

---
+++ scripts/database.js
@@ -0,0 +1,12 @@
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            species: "Bart's species",
+            food: "crustaceans",
+            size: 3,
+            location: "where harvested",
+            image: "link" //can we add an image to an object?
+        }
+    ]
+}
