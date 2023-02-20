d66d6f6ea4c502d3fe1015c59646c74d55e6b5b3
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-02-20 12:04:15 -0500

    uploading export/import code

--- scripts/database.js
+++ scripts/database.js
@@ -1,12 +1,57 @@
 const database = {
     fish: [
         {
-            name: "Bart",
+            name: "Angela",
             species: "Angel Fish",
             food: "crustaceans",
+            size: 5,
+            location: "Sydney, Australia",
+            image: "link" //can we add an image to an object?
+        },
+        {
+            name: "Bart",
+            species: "Barramundi",
+            food: "insects, crustaceans, other fish",
+            size: 12,
+            location: "Tinaroo Falls Dam, Australia",
+            image: "link" //can we add an image to an object?
+        },
+        {
+            name: "Carl",
+            species: "Cuttlefish",
+            food: "crustaceans",
             size: 3,
-            location: "where harvested",
+            location: "Bahamas",
+            image: "link" //can we add an image to an object?
+        },
+        {
+            name: "Davy",
+            species: "Dover Sole",
+            food: "worms",
+            size: 9,
+            location: "Mississippi",
+            image: "link" //can we add an image to an object?
+        },
+        {
+            name: "Ellie",
+            species: "Eel",
+            food: "fish, plankton",
+            size: 18,
+            location: "Mexico",
+            image: "link" //can we add an image to an object?
+        },
+        {
+            name: "George",
+            species: "Gooseneck",
+            food: "plants",
+            size: 7,
+            location: "Lake Michigan",
             image: "link" //can we add an image to an object?
         }
     ]
+}
+
+
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
 }
